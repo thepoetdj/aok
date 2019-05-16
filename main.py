4a4da25484470eda47e8a16bb1ef3dc45d0f6c86
Dhruv Joshi <joshidhruv997@gmail.com>
2019-05-16 21:20:19 +0530

    prepared a basic skeleton for join command with options and arguments

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import click
 
-@click.command()
+@click.group()
 @click.version_option()
 def aok():
     """A utility to split and/or join audio files.
     """
+
+@aok.command()
+@click.option('-o', '--output', help='Output file with absolute or relative path',
+                type=click.Path(exists=True), nargs=1, required=False)
+@click.argument('input', type=click.File(), nargs=-1, required=True)
+def join(output, input):
+    """Merge two or more audio files.
+    """
+    for audio_file in input:
+        # merge audio files to specified output file
+        pass
